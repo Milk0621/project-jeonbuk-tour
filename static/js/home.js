537d79ce3ed1237786b117c2530563547460f68a
김지선 <wltjs5360@gmail.com>
2025-03-26 00:15:05 +0900

    수우저엉

---
+++ static/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    let currentIndex = 0; 
+    const totalSlides = $(".slider").length; 
+
+    function moveSlide() {
+        currentIndex++;
+        if (currentIndex >= totalSlides) {
+            currentIndex = 0; // 마지막 슬라이드 이후 첫 번째로 돌아감
+        }
+        $(".slider_inner").css("transform", `translateX(${-currentIndex * 100}%)`);
+    }
+
+    setInterval(moveSlide, 3000); // 3초마다 슬라이드 전환
+});
