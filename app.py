1479e4b717ac6653a42d131ecf97b87f0a05b2f9
kjs <kim@example.com>
2025-03-25 17:38:34 +0900

    html css

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("header.html")
+    return render_template("home.html")
+
+@app.route("/region")
+def region():
+    return render_template("region.html")
+
+@app.route("/theme")
+def theme():
+    return render_template("theme.html")
 
 app.run(debug=True)
