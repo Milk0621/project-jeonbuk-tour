7ffc3593b61fca3aba67f5b13aa0468378a291cd
kjs <kim@example.com>
2025-03-26 15:29:52 +0900

    즐겨찾기 수정

--- dao/favorites_dao.py
+++ dao/favorites_dao.py
@@ -1,7 +1,7 @@
 import pymysql
 import sys
 sys.path.append(".")
-from vo.favorite_vo import FavoriteVO
+from vo.favorites_vo import FavoritesVO
 
 class FavoriteDao:
     def __init__(self):
@@ -17,18 +17,30 @@ class FavoriteDao:
 
     #추가
     def insert_favorite(self, uno, sno):
-        sql = "insert into favorite(uno, sno)values(%s, %s)"
+        sql = "insert into favorites(uno, sno)values(%s, %s)"
         self.cursor.execute(sql, (uno, sno))
         self.conn.commit()
 
     #삭제
     def delete_favorite(self, uno):
-        sql = "delete from favorite where uno = %s"
+        sql = "delete from favorites where uno = %s"
         self.cursor.execute(sql, (uno))
         self.conn.commit()
 
     def close(self):
         self.cursor.close()
         self.conn.close()
+
+    #조회
+    def selecte_favorite(self, uno):
+        sql = "select * from favorites where uno = %s"
+        self.cursor.execute(sql, (uno))
+        result = self.cursor.fetchall()
+        favorites = []
+        for favorite in result:
+            uno, sno = favorite
+            vo = FavoritesVO(uno, sno)
+            favorites.append(vo)
+        return favorites
 
 
